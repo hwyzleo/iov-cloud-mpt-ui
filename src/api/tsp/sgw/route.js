afa3cccb6355995d0214bf5cd513f7f3582aa182
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-12-04 18:12:15 +0800

    改进: 调整TSP服务网关路由管理路径

--- src/api/tsp/sgw/route.js
+++ src/api/tsp/sgw/route.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询路由列表
 export function listRoute(query) {
   return request({
-    url: '/tsp-sgw/route/list',
+    url: '/system/tsp-sgw-route/list',
     method: 'get',
     params: query
   })
@@ -12,7 +12,7 @@ export function listRoute(query) {
 // 查询路由详细
 export function getRoute(routeId) {
   return request({
-    url: '/tsp-sgw/route/' + routeId,
+    url: '/system/tsp-sgw-route/' + routeId,
     method: 'get'
   })
 }
@@ -20,7 +20,7 @@ export function getRoute(routeId) {
 // 新增路由
 export function addRoute(data) {
   return request({
-    url: '/tsp-sgw/route',
+    url: '/system/tsp-sgw-route',
     method: 'post',
     data: data
   })
@@ -29,7 +29,7 @@ export function addRoute(data) {
 // 修改路由
 export function updateRoute(data) {
   return request({
-    url: '/tsp-sgw/route',
+    url: '/system/tsp-sgw-route',
     method: 'put',
     data: data
   })
@@ -38,7 +38,7 @@ export function updateRoute(data) {
 // 删除路由
 export function delRoute(routeId) {
   return request({
-    url: '/tsp-sgw/route/' + routeId,
+    url: '/system/tsp-sgw-route/' + routeId,
     method: 'delete'
   })
 }
