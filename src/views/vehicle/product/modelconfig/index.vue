61416308d7a204d24d58043aca69f03abd53ebd6
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-12-08 23:23:32 +0800

    改进: 优化车型配置管理

--- src/views/vehicle/product/modelconfig/index.vue
+++ src/views/vehicle/product/modelconfig/index.vue
@@ -129,7 +129,7 @@
           ></el-switch>
         </template>
       </el-table-column>
-      <el-table-column label="排序" prop="sort" align="center" width="80"/>
+      <el-table-column label="排序" prop="sort" align="center" width="90"/>
       <el-table-column label="创建时间" align="center" prop="createTime" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
