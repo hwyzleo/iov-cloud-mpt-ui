2321159a86860461589ec51f6467b7ee85b70679
hwyz_leo <hwyz.leo.ye@gmail.com>
2025-09-16 09:46:56 +0800

    功能: 添加FOTA文章管理功能
    - 新增文章管理相关的 API 接口
    - 实现文章列表查询、文章详情获取、文章新增、修改和删除功能
    - 集成 TinyMCE 富文本编辑器
    - 添加文章管理的路由和权限控制

---
+++ src/views/ota/fota/article/index.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch">
+      <el-form-item label="文章标题" prop="title">
+        <el-input
+          v-model="queryParams.title"
+          placeholder="请输入文章标题"
+          clearable
+          style="width: 150px"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="文章类型" prop="type">
+        <el-select
+          v-model="queryParams.type"
+          placeholder="文章类型"
+          clearable
+          style="width: 140px"
+        >
+          <el-option key="1" label="活动条款" value="1" />
+          <el-option key="2" label="升级须知" value="2" />
+          <el-option key="3" label="隐私协议" value="3" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="创建时间">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['ota:fota:article:add']"
+        >新增
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['ota:fota:article:edit']"
+        >修改
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['ota:fota:article:remove']"
+        >删除
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['ota:fota:article:export']"
+        >导出
+        </el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="articleList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="文章标题" prop="title"/>
+      <el-table-column label="文章类型" prop="type" width="120" align="center">
+        <template slot-scope="scope">
+          <span v-if="scope.row.type === 1">活动条款</span>
+          <span v-else-if="scope.row.type === 2">升级须知</span>
+          <span v-else-if="scope.row.type === 3">隐私协议</span>
+          <span v-else>未知</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="200" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['ota:fota:article:edit']"
+          >修改
+          </el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['ota:fota:article:remove']"
+          >删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改文章对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="文章标题" prop="title">
+          <el-input v-model="form.title" placeholder="请输入文章标题"/>
+        </el-form-item>
+        <el-form-item label="文章类型" prop="type">
+          <el-select
+            v-model="form.type"
+            placeholder="文章类型"
+            clearable
+          >
+            <el-option key="1" label="活动条款" value="1"/>
+            <el-option key="2" label="升级须知" value="2"/>
+            <el-option key="3" label="隐私协议" value="3"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="文章内容" prop="content">
+          <Editor
+            v-model="form.content"
+            :init="{
+          height: 400,
+          menubar: false,
+          plugins: 'advlist autolink lists image',
+          toolbar: 'bold italic | alignleft | image'
+        }"
+          />
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.description" type="textarea" placeholder="请输入内容"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import {
+  addArticle,
+  delArticle,
+  getArticle,
+  listArticle,
+  updateArticle
+} from "@/api/ota/fota/article";
+import Editor from '@tinymce/tinymce-vue';
+
+export default {
+  name: "FotaArticle",
+  dicts: [],
+  components: { Editor },
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 基线表格数据
+      articleList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 日期范围
+      dateRange: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10
+      },
+      // 表单参数
+      form: {
+        content: ''
+      },
+      // 表单校验
+      rules: {
+        title: [
+          {required: true, message: "文章标题不能为空", trigger: "blur"}
+        ],
+        type: [
+          {required: true, message: "文章类型不能为空", trigger: "blur"}
+        ]
+      },
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询文章列表 */
+    getList() {
+      this.loading = true;
+      listArticle(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+          this.articleList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        }
+      );
+    },
+    /** 取消按钮 */
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    /** 表单重置 */
+    reset() {
+      this.form = {
+        title: undefined,
+        type: undefined,
+        content: undefined,
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.dateRange = [];
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    /** 多选框选中数据 */
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length != 1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加文章";
+      this.form = {};
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const articleId = row.id || this.ids
+      getArticle(articleId).then(response => {
+        this.form = response.data;
+        this.open = true;
+      });
+      this.title = "修改文章";
+    },
+    /** 提交按钮 */
+    submitForm: function () {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id !== undefined) {
+            updateArticle(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addArticle(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const articleIds = row.id || this.ids;
+      this.$modal.confirm('是否确认删除文章ID为"' + articleIds + '"的数据项？').then(function () {
+        return delArticle(articleIds);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {
+      });
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('ota-fota/article/export', {
+        ...this.queryParams
+      }, `article_${new Date().getTime()}.xlsx`)
+    },
+  }
+};
+</script>
