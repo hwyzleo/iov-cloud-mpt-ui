0e8c4f363e406f51a42b38e84b851138d0204ae8
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-01-15 10:05:43 +0800

    功能: 添加兼容零件号管理功能并修复固定配置字权限标识

---
+++ src/api/ota/baseline/compatiblepn.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询兼容零件号列表
+export function listCompatiblePn(query) {
+  return request({
+    url: '/ota-baseline/mpt/compatiblePn/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询兼容零件号详细
+export function getCompatiblePn(compatiblePnId) {
+  return request({
+    url: '/ota-baseline/mpt/compatiblePn/' + compatiblePnId,
+    method: 'get'
+  })
+}
+
+// 新增兼容零件号
+export function addCompatiblePn(data) {
+  return request({
+    url: '/ota-baseline/mpt/compatiblePn',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改兼容零件号
+export function updateCompatiblePn(data) {
+  return request({
+    url: '/ota-baseline/mpt/compatiblePn',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除兼容零件号
+export function delCompatiblePn(compatiblePnIds) {
+  return request({
+    url: '/ota-baseline/mpt/compatiblePn/' + compatiblePnIds,
+    method: 'delete'
+  })
+}
--- src/views/ota/baseline/compatiblepn/index.vue
+++ src/views/ota/baseline/compatiblepn/index.vue
@@ -50,7 +50,7 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['ota:baseline:fixedConfigWord:add']"
+          v-hasPermi="['ota:baseline:compatiblePn:add']"
         >新增
         </el-button>
       </el-col>
@@ -62,7 +62,7 @@
           size="mini"
           :disabled="single"
           @click="handleUpdate"
-          v-hasPermi="['ota:baseline:fixedConfigWord:edit']"
+          v-hasPermi="['ota:baseline:compatiblePn:edit']"
         >修改
         </el-button>
       </el-col>
@@ -74,7 +74,7 @@
           size="mini"
           :disabled="multiple"
           @click="handleDelete"
-          v-hasPermi="['ota:baseline:fixedConfigWord:remove']"
+          v-hasPermi="['ota:baseline:compatiblePn:remove']"
         >删除
         </el-button>
       </el-col>
@@ -85,20 +85,25 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-          v-hasPermi="['ota:baseline:fixedConfigWord:export']"
+          v-hasPermi="['ota:baseline:compatiblePn:export']"
         >导出
         </el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="fixedConfigWordList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="list" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center"/>
       <el-table-column label="ECU设备" prop="ecu" width="200"/>
-      <el-table-column label="软件零件号" prop="softwarePn" width="150"/>
-      <el-table-column label="分类" prop="type" width="150" align="center">
+      <el-table-column label="分类" prop="type" width="150">
+        <template slot-scope="scope">
+          <span>{{ scope.row.type === 1 ? '软件零件号' : '硬件零件号' }}</span>
+        </template>
       </el-table-column>
-      <el-table-column label="描述" prop="description"/>
+      <el-table-column label="零件号" prop="pn" width="150"/>
+      <el-table-column label="兼容零件号" prop="compatiblePn">
+      </el-table-column>
+      <el-table-column label="描述" prop="description" width="150"/>
       <el-table-column label="创建时间" align="center" prop="createTime" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
@@ -111,23 +116,15 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['ota:baseline:fixedConfigWord:edit']"
+            v-hasPermi="['ota:baseline:compatiblePn:edit']"
           >修改
-          </el-button>
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleDetail(scope.row)"
-            v-hasPermi="['ota:baseline:fixedConfigWord:query']"
-          >查看明细列表
           </el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['ota:baseline:fixedConfigWord:remove']"
+            v-hasPermi="['ota:baseline:compatiblePn:remove']"
           >删除
           </el-button>
         </template>
@@ -142,7 +139,7 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改升级任务对话框 -->
+    <!-- 添加或修改对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="140px">
         <el-form-item label="ECU设备" prop="ecu">
@@ -160,16 +157,21 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="软件零件号" prop="softwarePn">
-          <el-input v-model="form.softwarePn" placeholder="请输入软件零件号"/>
-        </el-form-item>
         <el-form-item label="分类" prop="type">
           <el-select
             v-model="form.type"
             placeholder="分类"
             clearable
           >
+            <el-option label="软件零件号" :value="1" />
+            <el-option label="硬件零件号" :value="2" />
           </el-select>
+        </el-form-item>
+        <el-form-item label="零件号" prop="pn">
+          <el-input v-model="form.pn" placeholder="请输入零件号"/>
+        </el-form-item>
+        <el-form-item label="兼容零件号" prop="pn">
+          <el-input v-model="form.compatiblePn" placeholder="请输入兼容零件号"/>
         </el-form-item>
         <el-form-item label="描述">
           <el-input v-model="form.description" type="textarea" placeholder="请输入描述内容"></el-input>
@@ -185,18 +187,18 @@
 
 <script>
 import {
-  addFixedConfigWord,
-  delFixedConfigWord,
-  getFixedConfigWord,
-  listFixedConfigWord,
-  updateFixedConfigWord,
-} from "@/api/ota/baseline/fixedconfigword";
+  addCompatiblePn,
+  delCompatiblePn,
+  getCompatiblePn,
+  listCompatiblePn,
+  updateCompatiblePn,
+} from "@/api/ota/baseline/compatiblepn";
 import {
   listAllEcu
 } from "@/api/ota/baseline/ecu";
 
 export default {
-  name: "FixedConfigWord",
+  name: "CompatiblePn",
   dicts: [],
   data() {
     return {
@@ -212,8 +214,7 @@ export default {
       showSearch: true,
       // 总条数
       total: 0,
-      // 升级任务表格数据
-      fixedConfigWordList: [],
+      list: [],
       ecuList: [],
       // 弹出层标题
       title: "",
@@ -233,8 +234,14 @@ export default {
         ecu: [
           {required: true, message: "ECU设备不能为空", trigger: "blur"}
         ],
-        softwarePn: [
-          {required: true, message: "软件零件号不能为空", trigger: "blur"}
+        type: [
+          {required: true, message: "分类不能为空", trigger: "blur"}
+        ],
+        pn: [
+          {required: true, message: "零件号不能为空", trigger: "blur"}
+        ],
+        compatiblePn: [
+          {required: true, message: "兼容零件号不能为空", trigger: "blur"}
         ]
       },
     };
@@ -244,11 +251,10 @@ export default {
     this.getList();
   },
   methods: {
-    /** 查询固定配置字列表 */
     getList() {
       this.loading = true;
-      listFixedConfigWord(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
-          this.fixedConfigWordList = response.rows;
+      listCompatiblePn(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+          this.list = response.rows;
           this.total = response.total;
           this.loading = false;
         }
@@ -259,26 +265,6 @@ export default {
           this.ecuList = response.data;
         }
       );
-    },
-    queryActivity(queryString, cb) {
-      listActivity({
-        name: queryString,
-        state: 5
-      }).then(response => {
-        if (response.rows && response.rows.length > 0) {
-          const suggestions = response.rows.map(item => {
-            return {
-              value: item.title,
-              ...item
-            };
-          });
-          cb(suggestions);
-        } else {
-          cb([]);
-        }
-      }).catch(() => {
-        cb([]);
-      });
     },
     /** 取消按钮 */
     cancel() {
@@ -313,38 +299,31 @@ export default {
     handleAdd() {
       this.reset();
       this.open = true;
-      this.title = "添加固定配置字";
+      this.title = "添加兼容零件号";
       this.form = {};
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
-      const fixedConfigWordId = row.id || this.ids
-      getFixedConfigWord(fixedConfigWordId).then(response => {
+      const compatiblePnId = row.id || this.ids
+      getCompatiblePn(compatiblePnId).then(response => {
         this.form = response.data;
         this.open = true;
       });
-      this.title = "修改固定配置字";
-    },
-    /** 明细列表按钮操作 */
-    handleDetail(row) {
-      this.$router.push({
-        path: "/ota/baseline/fixedConfigWordDetail",
-        query: { id: row.id }
-      });
+      this.title = "修改兼容零件号";
     },
     /** 提交按钮 */
     submitForm: function () {
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.form.id !== undefined) {
-            updateFixedConfigWord(this.form).then(response => {
+            updateCompatiblePn(this.form).then(response => {
               this.$modal.msgSuccess("修改成功");
               this.open = false;
               this.getList();
             });
           } else {
-            addFixedConfigWord(this.form).then(response => {
+            addCompatiblePn(this.form).then(response => {
               this.$modal.msgSuccess("新增成功");
               this.open = false;
               this.getList();
@@ -355,9 +334,9 @@ export default {
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      const fixedConfigWordIds = row.id || this.ids;
-      this.$modal.confirm('是否确认删除固定配置字ID为"' + fixedConfigWordIds + '"的数据项？').then(function () {
-        return delFixedConfigWord(fixedConfigWordIds);
+      const compatiblePnIds = row.id || this.ids;
+      this.$modal.confirm('是否确认删除兼容零件号ID为"' + compatiblePnIds + '"的数据项？').then(function () {
+        return delCompatiblePn(compatiblePnIds);
       }).then(() => {
         this.getList();
         this.$modal.msgSuccess("删除成功");
@@ -366,9 +345,9 @@ export default {
     },
     /** 导出按钮操作 */
     handleExport() {
-      this.download('ota-baseline/fixedConfigWord/export', {
+      this.download('ota-baseline/compatiblePn/export', {
         ...this.queryParams
-      }, `fixed_config_word_${new Date().getTime()}.xlsx`)
+      }, `compatible_pn_${new Date().getTime()}.xlsx`)
     },
   }
 };
