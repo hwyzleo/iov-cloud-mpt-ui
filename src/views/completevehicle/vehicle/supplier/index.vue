dd421fc670f82e54b96ad2d4a58156389c16cc93
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-01-23 15:57:51 +0800

    功能: 新增供应商管理功能

---
+++ src/views/completevehicle/vehicle/supplier/index.vue
@@ -0,0 +1,340 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch">
+      <el-form-item label="供应商代码" prop="code">
+        <el-input
+          v-model="queryParams.code"
+          placeholder="请输入供应商代码"
+          clearable
+          style="width: 150px"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="供应商名称" prop="name">
+        <el-input
+          v-model="queryParams.name"
+          placeholder="请输入供应商名称"
+          clearable
+          style="width: 200px"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="创建时间">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['completeVehicle:vehicle:supplier:add']"
+        >新增
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['completeVehicle:vehicle:supplier:edit']"
+        >修改
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['completeVehicle:vehicle:supplier:remove']"
+        >删除
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['completeVehicle:vehicle:supplier:export']"
+        >导出
+        </el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="list" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="供应商代码" prop="code" width="100"/>
+      <el-table-column label="供应商名称" prop="name"/>
+      <el-table-column label="供应商名称缩写" prop="nameShort" width="250"/>
+      <el-table-column label="供应商英文名称" prop="nameEn"/>
+      <el-table-column label="是否启用" align="center" width="100">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.enable"
+            :active-value="true"
+            :inactive-value="false"
+          ></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="排序" prop="sort" align="center" width="60"/>
+      <el-table-column label="创建时间" align="center" prop="createTime" width="160">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['completeVehicle:vehicle:supplier:edit']"
+          >修改
+          </el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['completeVehicle:vehicle:supplier:remove']"
+          >删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改供应商对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="供应商代码" prop="code">
+          <el-input v-model="form.code" :readonly="form.id !== undefined" placeholder="请输入供应商代码"/>
+        </el-form-item>
+        <el-form-item label="供应商名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入供应商名称"/>
+        </el-form-item>
+        <el-form-item label="供应商名称缩写" prop="nameShort">
+          <el-input v-model="form.nameShort" placeholder="请输入供应商名称缩写"/>
+        </el-form-item>
+        <el-form-item label="供应商英文名称" prop="nameEn">
+          <el-input v-model="form.nameEn" placeholder="请输入供应商英文名称"/>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.enable">
+            <el-radio
+              :label="true"
+            >启用
+            </el-radio>
+            <el-radio
+              :label="false"
+            >停用
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="排序" prop="sort">
+          <el-input-number v-model="form.sort" controls-position="right" :min="0"/>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.description" type="textarea" placeholder="请输入内容"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import {
+  addSupplier,
+  delSupplier,
+  getSupplier,
+  listSupplier,
+  updateSupplier
+} from "@/api/completevehicle/vehicle/supplier";
+
+export default {
+  name: "Supplier",
+  dicts: [],
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 表格数据
+      list: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 日期范围
+      dateRange: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        code: [
+          {required: true, message: "供应商代码不能为空", trigger: "blur"}
+        ],
+        name: [
+          {required: true, message: "供应商名称不能为空", trigger: "blur"}
+        ]
+      },
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询供应商列表 */
+    getList() {
+      this.loading = true;
+      listSupplier(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+          this.list = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        }
+      );
+    },
+    /** 取消按钮 */
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    /** 表单重置 */
+    reset() {
+      this.form = {
+        code: undefined,
+        name: undefined,
+        nameEn: undefined,
+        enable: true,
+        sort: 99
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.dateRange = [];
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    /** 多选框选中数据 */
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length != 1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加供应商";
+      this.form = {
+        enable: true,
+        sort: 99
+      };
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const supplierId = row.id || this.ids
+      getSupplier(supplierId).then(response => {
+        this.form = response.data;
+        this.open = true;
+      });
+      this.title = "修改供应商";
+    },
+    /** 提交按钮 */
+    submitForm: function () {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id !== undefined) {
+            updateSupplier(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addSupplier(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const supplierIds = row.id || this.ids;
+      this.$modal.confirm('是否确认删除供应商ID为"' + supplierIds + '"的数据项？').then(function () {
+        return delSupplier(supplierIds);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {
+      });
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('tsp-vmd/supplier/export', {
+        ...this.queryParams
+      }, `supplier_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
